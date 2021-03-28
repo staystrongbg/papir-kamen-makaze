e76a5fa50e891d91c3e4126bbb27bdadd9c704b6
Zoran <staystrongbg@gmail.com>
2021-03-28 16:39:08 +0200

    functional changes

--- script.js
+++ script.js
@@ -1,40 +1,45 @@
-const choice = ['scissors', 'rock', 'paper']
-//ai
-const computerSelection = () => {
-  let aiChoice
-  const randomNum = Math.floor(Math.random() * 3)
-  randomNum === 0
-    ? (aiChoice = choice[0])
-    : randomNum === 1
-    ? (aiChoice = choice[1])
-    : (aiChoice = choice[2])
-  return aiChoice
-}
-let comp = computerSelection()
-console.log(comp)
+const game = () => {
+  const choice = ['scissors', 'rock', 'paper']
+  //ai
+  const computerSelection = () => {
+    let aiChoice
+    const randomNum = Math.floor(Math.random() * 3)
+    randomNum === 0
+      ? (aiChoice = choice[0])
+      : randomNum === 1
+      ? (aiChoice = choice[1])
+      : (aiChoice = choice[2])
+    return aiChoice
+  }
+  let comp = computerSelection()
+  console.log(comp)
 
-//player
-const playerSelection = () => {
-  let playersChoice = prompt('chose your weapon?')
-  playersChoice = playersChoice.toLowerCase()
-  console.log(playersChoice)
-  return playersChoice
-}
-let player = playerSelection()
+  //player
+  const playerSelection = () => {
+    let playersChoice = prompt('chose your weapon?')
+    playersChoice = playersChoice.toLowerCase()
+    console.log(playersChoice)
+    return playersChoice
+  }
+  let player = playerSelection()
 
-const playRound = () => {
-  if (
-    (comp === choice[0] && player === choice[1]) ||
-    (comp === choice[2] && player === choice[0]) ||
-    (comp === choice[1] && player == choice[2])
-  ) {
-    console.log('player won!')
-  } else if (
-    (comp === choice[0] && player === choice[0]) ||
-    (comp === choice[2] && player === choice[2]) ||
-    (comp === choice[1] && player === choice[1])
-  ) {
-    console.log('Tied!')
-  } else console.log('AI won!')
+  const playRound = () => {
+    if (
+      (comp === choice[0] && player === choice[1]) ||
+      (comp === choice[2] && player === choice[0]) ||
+      (comp === choice[1] && player == choice[2])
+    ) {
+      console.log('player won!')
+    } else if (
+      (comp === choice[0] && player === choice[0]) ||
+      (comp === choice[2] && player === choice[2]) ||
+      (comp === choice[1] && player === choice[1])
+    ) {
+      console.log('Tied!')
+    } else console.log('AI won!')
+  }
+  console.log(playRound())
 }
-console.log(playRound())
+for (i = 0; i <= 5; i++) {
+  game()
+}
