44ed3abc59be17a9774df2d93e00a9cd52ef7d1f
Zoran <staystrongbg@gmail.com>
2021-03-28 21:37:38 +0200

    major changes

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const choice = ['scissors', 'rock', 'paper'];
+const result = document.querySelector('.result');
+const score = document.querySelector('.score');
 let aiScore = (playerScore = 0);
 //ai
 const computerSelection = () => {
@@ -8,7 +10,7 @@ const computerSelection = () => {
     : aiChoice == 1
     ? (aiChoice = choice[1])
     : (aiChoice = choice[2]);
-  console.log(aiChoice);
+  // console.log(aiChoice);
   return aiChoice;
 };
 
@@ -16,7 +18,7 @@ const computerSelection = () => {
 const playerSelection = () => {
   let playersChoice = prompt('chose your weapon?');
   playersChoice = playersChoice.toLowerCase();
-  console.log(playersChoice);
+  // console.log(playersChoice);
   return playersChoice;
 };
 
@@ -28,11 +30,16 @@ const playRound = (computerSelection, playerSelection) => {
     (computerSelection == choice[1] && playerSelection == choice[2])
   ) {
     console.log('player won!');
+    result.innerHTML = `<p>Player wins!</p>`;
+
     playerScore++;
   } else if (computerSelection === playerSelection) {
-    console.log('Tied!');
+    // console.log('Tied!');
+    result.innerHTML = `<p>Tied!</p>`;
   } else {
-    console.log('AI won!');
+    // console.log('AI won!');
+    result.innerHTML = `<p>AI wins!</p>`;
+
     aiScore++;
   }
 };
@@ -40,4 +47,5 @@ for (i = 0; i <= 4; i++) {
   console.log(playRound(computerSelection(), playerSelection()));
   console.log(` player score is: ${playerScore}`);
   console.log(` ai score is: ${aiScore}`);
+  score.textContent = `player: ${playerScore} / Ai: ${aiScore}`;
 }
